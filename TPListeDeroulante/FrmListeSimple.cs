4a72a9a4d59b6ea48127c0e343e1223695680da1
Dasp <dasporal@gmail.com>
2016-04-20 08:42:35 +0200

    ajout d'item et changement de mode de sélection foncitonnel

--- TPListeDeroulante/FrmListeSimple.cs
+++ TPListeDeroulante/FrmListeSimple.cs
@@ -10,11 +10,50 @@ using System.Windows.Forms;
 
 namespace TPListeDeroulante
 {
-    public partial class Form1 : Form
+    public partial class FrmListeSimple : Form
     {
-        public Form1()
+        public FrmListeSimple()
         {
             InitializeComponent();
+        }
+
+        private void chkLocked_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkLocked.Checked)
+                lstItems.SelectionMode = SelectionMode.None;
+            else
+                lstItems.SelectionMode = SelectionMode.One;
+        }
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            if (txtSaisie.Text!="")
+                lstItems.Items.Add(txtSaisie.Text);
+            else
+                MessageBox.Show("Rien à ajouter dans la liste");
+        }
+
+        private void rdBtn1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdBtn1.Checked)
+                lstItems.SelectionMode= SelectionMode.One;
+        }
+
+        private void rdBtn2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdBtn2.Checked)
+                lstItems.SelectionMode = SelectionMode.MultiSimple;
+        }
+
+        private void rdBtn3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdBtn3.Checked)
+                lstItems.SelectionMode= SelectionMode.MultiExtended;
+        }
+
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            lstItems.Items.Remove(lstItems.SelectedItems);
         }
     }
 }
